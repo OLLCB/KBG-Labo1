4a236d53a108504cb53d20348a99ce07663fb245
OLLCB <98758868+OLLCB@users.noreply.github.com>
2022-09-13 13:33:19 -0400

    Clean up code

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -31,32 +31,62 @@ module.exports =
         constructor(HttpContext) {
             super(HttpContext);
         }
+        error(message) { // Send error message 
+            this.HttpContext.path.params.error = message;
+            this.HttpContext.response.JSON(this.HttpContext.path.params);
+        }
+        result(value) { // Send result
+            this.HttpContext.path.params.value = value;
+            this.HttpContext.response.JSON(this.HttpContext.path.params);
+        }
+        sendHelpPage(accessPath) {
+            let helpPagePath = path.join(process.cwd(), accessPath);
+            let content = fs.readFileSync(helpPagePath);
+            this.HttpContext.response.content("text/html", content); // Sert la page HTML
+        }
+        valideateOperator(op) {
+            let singleNumberOperators = ["!", "p", "np"];
+            let doubleNumberOperators = [" ", "-", "*", "/", "%"];
+            let valide = true;
+            // Si 'op' n'est pas une opération valide ou est manquant
+            if (!singleNumberOperators.includes(op) && !doubleNumberOperators.includes(op)) {
+                valide = false;
+            }
+            return valide;
+        }
+        checkIfMissingOrNaN(value){
+            let error = false;
+            if(!value || isNaN(value))
+                error = true;
+            return error;
+        }
+        tooManyOperators(limit){
+            let tooMany = false;
+            if(Object.keys(this.HttpContext.path.params).length > limit)
+                tooMany = true;
+            return tooMany;
+        }
         get() {
             if (this.HttpContext.path.queryString == '?' || !this.HttpContext.path.queryString) {
                 // Afficher la page qui affiche les commandes possible
                 // Send helpPage.html
-                let helpPagePath = path.join(process.cwd(), "wwwroot/helpPages/mathsServiceHelp.html");
-                let content = fs.readFileSync(helpPagePath);
-                this.HttpContext.response.content("text/html", content); // Sert la page HTML
+                this.sendHelpPage("wwwroot/helpPages/mathsServiceHelp.html");
             }
             else { // Si on veut faire une opération (queryString 'op' existe)
                 let singleNumberOperators = ["!", "p", "np"];
                 let doubleNumberOperators = [" ", "-", "*", "/", "%"];
                 let op = this.HttpContext.path.params.op;
                 // Si 'op' n'est pas une opération valide ou est manquant
-                if (!singleNumberOperators.includes(op) && !doubleNumberOperators.includes(op)) {
-                    this.HttpContext.path.params.error = "Parameter 'op' is missing or invalid!";
-                    this.HttpContext.response.JSON(this.HttpContext.path.params);
+                if (!this.valideateOperator(this.HttpContext.path.params.op)) {
+                    this.error("Parameter 'op' is missing or invalid!");
                 }
                 else {
                     if (singleNumberOperators.includes(op)) { // Vérifier si le nombre de paramètres nécessaire à faire les opérations existe.
-                        if (!this.HttpContext.path.params.n || isNaN(this.HttpContext.path.params.n)) {
-                            this.HttpContext.path.params.error = "'n' is missing or not a number!";
-                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                        if (this.checkIfMissingOrNaN(this.HttpContext.path.params.n)) {
+                            this.error("'n' is missing or not a number!");
                         }
-                        else if(Object.keys(this.HttpContext.path.params).length > 2){ // S'il y a trop de paramètres dans la requête (Besoin de 'op' et 'n' seulement)
-                            this.HttpContext.path.params.error = "Too many parameters!";
-                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                        else if (this.tooManyOperators(2)) { // S'il y a trop de paramètres dans la requête (Besoin de 'op' et 'n' seulement)
+                            this.error("Too many parameters!");
                         }
                         else { // Si 'op' est valide et 'n' est un nombre valide.
                             let n = this.HttpContext.path.params.n;
@@ -65,40 +95,32 @@ module.exports =
                                 case "!":
                                     if (n >= 0) {
                                         value = factorial(parseInt(n));
-                                        this.HttpContext.path.params.value = value;
-                                        this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                        this.result(value);
                                     }
                                     else { // Si n est < 0
-                                        this.HttpContext.path.params.error = "'n' must be a positive number";
-                                        this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                        this.error("'n' must be a positive number");
                                     }
                                     break;
                                 case "p":
                                     value = isPrime(n);
-                                    this.HttpContext.path.params.value = value;
-                                    this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                    this.result(value);
                                     break;
                                 case "np":
                                     value = findPrime(n);
-                                    this.HttpContext.path.params.value = value;
-                                    this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                    this.result(value);
                                     break;
                             }
                         }
                     }
                     else if (doubleNumberOperators.includes(op)) { // Si est un opérateur à deux nombres
-                        if (!this.HttpContext.path.params.x || isNaN(this.HttpContext.path.params.x)) { // Si 'X' est invalide ou inexistant
-                            this.HttpContext.path.params.error = "'x' is missing or not a number!";
-                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                        if (this.checkIfMissingOrNaN(this.HttpContext.path.params.x)) { // Si 'X' est invalide ou inexistant
+                            this.error("'x' is missing or not a number!");
                         }
-                        else if (!this.HttpContext.path.params.y || isNaN(this.HttpContext.path.params.y)) // Si 'Y' est invalide ou inexistant
-                        {
-                            this.HttpContext.path.params.error = "'y' is missing or not a number!";
-                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                        else if (this.checkIfMissingOrNaN(this.HttpContext.path.params.y)){ // Si 'Y' est invalide ou inexistant
+                            this.error("'y' is missing or not a number!");
                         }
-                        else if(Object.keys(this.HttpContext.path.params).length > 3){ // S'il y a trop de paramètres dans la requête (Besoin de 'op', 'x' et 'y' seulement)
-                            this.HttpContext.path.params.error = "Too many parameters!";
-                            this.HttpContext.response.JSON(this.HttpContext.path.params);
+                        else if (this.tooManyOperators(3)) { // S'il y a trop de paramètres dans la requête (Besoin de 'op', 'x' et 'y' seulement)
+                            this.error("Too many parameters!");
                         }
                         else { // Si 'op', 'X' ET 'Y' sont tous valide
                             let x = parseInt(this.HttpContext.path.params.x);
@@ -107,44 +129,35 @@ module.exports =
                             switch (op) {
                                 case " ":
                                     value = x + y;
-                                    this.HttpContext.path.params.value = value;
-                                    this.HttpContext.path.params.op = "+";
-                                    this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                    this.HttpContext.path.params.op = "+"; // Remplacer le ' ' dans le querystring par le symbole '+', Problème unique à l'opérateur '+'
+                                    this.result(value);
                                     break;
                                 case "-":
                                     value = x - y;
-                                    this.HttpContext.path.params.value = value;
-                                    this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                    this.result(value);
                                     break;
                                 case "/":
                                     if (x == 0 || y == 0) {
                                         value = x / y;
-                                        //this.HttpContext.path.params.error = "Division by 0!";
-                                        this.HttpContext.path.params.value = "Division by 0!";
-                                        this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                        this.error("Division by 0!");
                                     }
                                     else {
                                         value = x / y;
-                                        this.HttpContext.path.params.value = value;
-                                        this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                        this.result(value);
                                     }
                                     break;
                                 case "*":
                                     value = x * y;
-                                    this.HttpContext.path.params.value = value;
-                                    this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                    this.result(value);
                                     break;
                                 case "%":
                                     if (x == 0 || y == 0) {
                                         value = x / y;
-                                        //this.HttpContext.path.params.error = "Division by 0!";
-                                        this.HttpContext.path.params.value = "Division by 0!";
-                                        this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                        this.error("Division by 0!");
                                     }
                                     else {
                                         value = x % y;
-                                        this.HttpContext.path.params.value = value;
-                                        this.HttpContext.response.JSON(this.HttpContext.path.params);
+                                        this.result(value);
                                     }
                                     break;
                             }
